01867c0237b510bd2ab47993daf13074e79cab0b
RYUJIKOYAMANO\ryuji <ryujikoyama95@gmail.com>
2024-02-19 22:34:14 +0900

    Add process that start default camera actoin

--- app/src/main/java/com/eotw95/samplepicture/MainActivity.kt
+++ app/src/main/java/com/eotw95/samplepicture/MainActivity.kt
@@ -1,6 +1,9 @@
 package com.eotw95.samplepicture
 
+import android.content.ActivityNotFoundException
+import android.content.Intent
 import android.os.Bundle
+import android.provider.MediaStore
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
@@ -13,8 +16,12 @@ import androidx.compose.ui.tooling.preview.Preview
 import com.eotw95.samplepicture.ui.theme.SamplePictureTheme
 
 class MainActivity : ComponentActivity() {
+    companion object {
+        private const val REQUEST_IMAGE_CAPTURE = 1
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        dispatchTakePictureIntent()
         setContent {
             SamplePictureTheme {
                 // A surface container using the 'background' color from the theme
@@ -24,6 +31,15 @@ class MainActivity : ComponentActivity() {
                 ) {
                 }
             }
+        }
+    }
+
+    private fun dispatchTakePictureIntent() {
+        val takePictureIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+        try {
+            startActivityForResult(takePictureIntent, REQUEST_PICTURE_CAPTURE)
+        } catch (e: ActivityNotFoundException) {
+            // do nothing
         }
     }
 }
